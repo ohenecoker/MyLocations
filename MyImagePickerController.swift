3de511148aec8f51316d8f55b8d12d1d7b48e7f6
ohenecoker <coker.christian@gmail.com>
2018-01-15 14:44:15 -0500

    Make table view in Locations screen black

---
+++ MyImagePickerController.swift
@@ -0,0 +1,15 @@
+//
+//  MyImagePickerController.swift
+//  MyLocations
+//
+//  Created by Christian Coker on 1/13/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class MyImagePickerController: UIImagePickerController {
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+}
