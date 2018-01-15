e7bd3887a2aed9692b35763eadde3e56259ab382
ohenecoker <coker.christian@gmail.com>
2018-01-15 16:56:16 -0500

    reduce the section header size.

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -52,6 +52,7 @@ class LocationsViewController: UITableViewController {
     super.viewDidLoad()
     performFetch()
     navigationItem.rightBarButtonItem = editButtonItem
+    tableView.sectionHeaderHeight = 30
   }
   
   deinit {
@@ -113,6 +114,27 @@ class LocationsViewController: UITableViewController {
         fatalCoreDataError(error)
       }
     }
+  }
+  
+  override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let labelRect = CGRect(x: 15, y: tableView.sectionHeaderHeight - 14,
+                           width: 300, height: 14)
+    let label = UILabel(frame: labelRect)
+    label.font = UIFont.boldSystemFont(ofSize: 11)
+    label.text = tableView.dataSource!.tableView!(tableView, titleForHeaderInSection: section)
+    label.textColor = UIColor(white: 1.0, alpha: 0.4)
+    label.backgroundColor = UIColor.clear
+    let separatorRect = CGRect(x: 15, y: tableView.sectionHeaderHeight - 0.5, width: tableView.bounds.size.width - 15, height: 0.5)
+    let separator = UIView(frame: separatorRect)
+    separator.backgroundColor = tableView.separatorColor
+    let viewRect = CGRect(x: 0, y: 0,
+                          width: tableView.bounds.size.width,
+                          height: tableView.sectionHeaderHeight)
+    let view = UIView(frame: viewRect)
+    view.backgroundColor = UIColor(white: 0, alpha: 0.85)
+    view.addSubview(label)
+    view.addSubview(separator)
+    return view
   }
 }
 
