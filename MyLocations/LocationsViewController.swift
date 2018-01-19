7a871a51a498c898bd0b85b1fc4f15ba1b191811
ohenecoker <coker.christian@gmail.com>
2018-01-19 10:48:16 -0500

    capitalize section header titles

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -92,7 +92,7 @@ class LocationsViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     let sectionInfo = fetchedResultsController.sections![section]
-    return sectionInfo.name
+    return sectionInfo.name.uppercased()
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
