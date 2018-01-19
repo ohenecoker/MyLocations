460e67335568310fd14b90ffca276a33a56988e2
ohenecoker <coker.christian@gmail.com>
2018-01-19 10:50:41 -0500

    Use default image if user does not tag a location with an image

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -72,7 +72,7 @@ class LocationCell: UITableViewCell {
     if location.hasPhoto, let image = location.photoImage {
       return image.resized(withBounds: CGSize(width: 52, height: 52))
     }
-    return UIImage()
+    return UIImage(named: "No Photo")!
   }
 }
 
